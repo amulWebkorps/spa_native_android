0f6e112e0046e24a045f26f5eab920bb9244f971
Rohit Mahajan <rohit@webkorps.com>
2023-09-07 18:37:21 +0530

    swipe left to right

--- app/src/main/java/com/example/spa/ui/auth/activity/MyTipsFragment.kt
+++ app/src/main/java/com/example/spa/ui/auth/activity/MyTipsFragment.kt
@@ -3,9 +3,11 @@ package com.example.spa.ui.auth.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import com.example.spa.R
 import com.example.spa.base.BaseFragment
 import com.example.spa.base.listener.TutorialScreen
@@ -18,6 +20,9 @@ class MyTipsFragment : BaseFragment() {
     private lateinit var binding: FragmentMyTipsBinding
     private lateinit var update: Button
     private lateinit var skip:Button
+    private var startX = 0
+    private var endX = 0
+    private var MIN_SWIPE_DISTANCE = 150 // Minimum distance for a swipe
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +40,32 @@ class MyTipsFragment : BaseFragment() {
 
         binding.textViewLanguage.setOnClickListener {
             tutorialListener?.replaceFragment(TutorialScreen.SELECT_LANGUAGE)
+        }
+        binding.finalMain.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> startX = event.x.toInt()
+                MotionEvent.ACTION_UP -> {
+                    endX = event.x.toInt()
+                    val deltaX = (endX - startX)
+                    //Toast.makeText(requireContext(),"xxxxx"+deltaX, Toast.LENGTH_LONG).show()
+                    // Check if the swipe distance is greater than the minimum threshold
+                    if (Math.abs(deltaX) >= MIN_SWIPE_DISTANCE) {
+
+                        //Toast.makeText(requireContext(),"hope", Toast.LENGTH_LONG).show()
+                        if (deltaX > 0) {
+                            goToNext()
+                            //Toast.makeText(requireContext(),"Right to left swipe", Toast.LENGTH_LONG).show()
+                            // Right to left swipe
+                            // Handle your action for this swipe direction
+                        } else {
+                            //Toast.makeText(requireContext(),"Left to right swipe", Toast.LENGTH_LONG).show()
+                            // Left to right swipe
+                            // Handle your action for this swipe direction
+                        }
+                    }
+                }
+            }
+            true
         }
         when (session.language) {
             getString(R.string.english) -> {
@@ -56,17 +87,22 @@ class MyTipsFragment : BaseFragment() {
 
     private fun handler(){
         binding.layoutBottom.setOnClickListener {
-            session.isNotFirstTime = true
-            if (session.isLogin){
-                val intent= Intent(requireContext(), HomeActivity::class.java)
-                startActivity(intent)
-                requireActivity().finishAffinity()
-            }
-            else {
-                val intent = Intent(requireContext(), AuthActivity::class.java)
-                startActivity(intent)
-                requireActivity().finishAffinity()
-            }
+            goToNext()
+
+        }
+    }
+
+    private fun goToNext() {
+        session.isNotFirstTime = true
+        if (session.isLogin){
+            val intent= Intent(requireContext(), HomeActivity::class.java)
+            startActivity(intent)
+            requireActivity().finishAffinity()
+        }
+        else {
+            val intent = Intent(requireContext(), AuthActivity::class.java)
+            startActivity(intent)
+            requireActivity().finishAffinity()
         }
     }
 }
